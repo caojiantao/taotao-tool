b3087618a9079e7bce5cc57207ea198bb8717ff1
caojiantao <caojiantao@zhuanzhuan.com>
2023-02-28 16:12:34 +0800

    增加图片上传

---
+++ src/main/java/com/taotao/tool/controller/UploadController.java
@@ -0,0 +1,44 @@
+package com.taotao.tool.controller;
+
+import com.taotao.tool.dto.resp.ApiResp;
+import com.taotao.tool.exception.ApiException;
+import com.taotao.tool.yml.UploadYml;
+import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("/upload")
+public class UploadController {
+
+    @Autowired
+    private UploadYml uploadYml;
+
+    @PostMapping("/image")
+    public ApiResp<String> uploadImage(@RequestPart MultipartFile file) {
+        UploadYml.Image image = uploadYml.getImage();
+        String filename = UUID.randomUUID() + ".jpg";
+        File outFile = new File(image.getPath(), filename);
+        try (InputStream is = file.getInputStream()) {
+            Thumbnails.of(is)
+                    .outputFormat("jpg")
+                    .outputQuality(image.getQuality())
+                    .scale(1)
+                    .toFile(outFile);
+        } catch (Exception e) {
+            log.error("act=uploadImage", e);
+            throw new ApiException(-1, e.getMessage());
+        }
+        return ApiResp.success(filename);
+    }
+}
---
+++ src/main/java/com/taotao/tool/yml/UploadYml.java
@@ -0,0 +1,19 @@
+package com.taotao.tool.yml;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "upload")
+public class UploadYml {
+
+    private Image image;
+
+    @Data
+    public static class Image {
+        private String path;
+        private Double quality;
+    }
+}

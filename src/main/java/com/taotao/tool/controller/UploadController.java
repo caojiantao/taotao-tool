87f983f324d454a6cc739b6832e540ca1c9ff096
caojiantao <caojiantao@zhuanzhuan.com>
2023-04-12 10:58:42 +0800

    图片上传支持“文件名不变”，“压缩选项”

--- src/main/java/com/taotao/tool/controller/UploadController.java
+++ src/main/java/com/taotao/tool/controller/UploadController.java
@@ -8,6 +8,7 @@ import com.taotao.tool.yml.UploadYml;
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -16,6 +17,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.UUID;
 
 @Slf4j
@@ -28,16 +31,25 @@ public class UploadController {
 
     @RequireLogin
     @PostMapping("/image")
-    public ApiResp<String> uploadImage(@RequestPart MultipartFile file) {
+    public ApiResp<String> uploadImage(@RequestPart MultipartFile file, boolean keepName, boolean compress) {
         UploadYml.Image image = uploadYml.getImage();
-        String filename = UUID.randomUUID() + ".jpg";
+        // 是否保持
+        String filename = keepName ? file.getOriginalFilename() : (UUID.randomUUID() + ".jpg");
+        assert filename != null;
         File outFile = new File(image.getPath(), filename);
-        try (InputStream is = file.getInputStream()) {
-            Thumbnails.of(is)
-                    .outputFormat("jpg")
-                    .outputQuality(image.getQuality())
-                    .scale(1)
-                    .toFile(outFile);
+        try (InputStream is = file.getInputStream();
+             OutputStream os = Files.newOutputStream(outFile.toPath())) {
+            if (compress) {
+                // 压缩
+                Thumbnails.of(is)
+                        .outputFormat("jpg")
+                        .outputQuality(image.getQuality())
+                        .scale(1)
+                        .toFile(outFile);
+            } else {
+                // 不压缩
+                StreamUtils.copy(is, os);
+            }
         } catch (Exception e) {
             log.error("act=uploadImage", e);
             throw new ApiException(-1, e.getMessage());

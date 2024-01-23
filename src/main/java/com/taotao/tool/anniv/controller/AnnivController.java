c05f2038a4fd1055aa5ef44c2e2fb089e79400a6
caojiantao <13437104137@163.com>
2024-01-23 21:07:12 +0800

    增加纪念日功能

--- src/main/java/com/taotao/tool/anniv/controller/AnnivController.java
+++ src/main/java/com/taotao/tool/anniv/controller/AnnivController.java
@@ -1,13 +1,16 @@
 package com.taotao.tool.anniv.controller;
 
+import com.taotao.tool.anniv.constant.EAnnivType;
+import com.taotao.tool.anniv.dto.AnnivDTO;
 import com.taotao.tool.anniv.model.Anniv;
 import com.taotao.tool.anniv.service.IAnnivService;
 import com.taotao.tool.common.dto.ApiResult;
-import com.taotao.tool.system.annotation.RequireLogin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -25,14 +28,30 @@ public class AnnivController {
     private IAnnivService annivService;
 
     @GetMapping("/listAnniv")
-    public ApiResult<List<Anniv>> listAnniv() {
-        List<Anniv> list = annivService.list();
+    public ApiResult<List<AnnivDTO>> listAnniv() {
+        List<AnnivDTO> list = annivService.list().stream()
+                .map(
+                        item -> AnnivDTO.builder()
+                                .id(item.getId())
+                                .annivType(EAnnivType.valueOf(item.getAnnivType()))
+                                .annivDate(item.getAnnivDate().toString())
+                                .lunar(item.getLunar())
+                                .remark(item.getRemark())
+                                .build()
+                )
+                .collect(Collectors.toList());
         return ApiResult.success(list);
     }
 
     @PostMapping("/saveAnniv")
-    public ApiResult<Void> saveAnniv(@RequestBody Anniv anniv) {
-        annivService.saveOrUpdate(anniv);
+    public ApiResult<Void> saveAnniv(@RequestBody AnnivDTO anniv) {
+        Anniv record = new Anniv();
+        record.setId(anniv.getId());
+        record.setAnnivType(anniv.getAnnivType().name());
+        record.setAnnivDate(LocalDate.parse(anniv.getAnnivDate()));
+        record.setLunar(anniv.getLunar());
+        record.setRemark(anniv.getRemark());
+        annivService.saveOrUpdate(record);
         return ApiResult.success();
     }
 
